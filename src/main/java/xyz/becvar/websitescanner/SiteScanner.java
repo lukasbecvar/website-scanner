79c3e039e3f1c3d0c3a923b18d45a6c6a8e59303
lordbecvold <lordbecvold@gmail.com>
2022-07-07 16:04:57 +0200

    Added new colors for http codes

--- src/main/java/xyz/becvar/websitescanner/SiteScanner.java
+++ src/main/java/xyz/becvar/websitescanner/SiteScanner.java
@@ -71,16 +71,40 @@ public class SiteScanner {
 
                         con.connect();
 
-                        if (con.getResponseCode() != 404) {
-                            Logger.log(file + ":" + url + "/" + line + " code: " + new String(String.valueOf(con.getResponseCode())));
-                        } else if (con.getResponseCode() != 301) {
+                        //Print path and code to console
+
+                        //Succesful
+                        if (con.getResponseCode() == 200 || con.getResponseCode() == 202 || con.getResponseCode() == 201) {
+                            Logger.log(ConsoleColors.CODES.ANSI_GREEN + file + ":" + url + "/" + line + " code: " + new String(String.valueOf(con.getResponseCode())));
+
+                        //Redirection
+                        } else if (con.getResponseCode() == 301 || con.getResponseCode() == 302 || con.getResponseCode() == 303 || con.getResponseCode() == 304 || con.getResponseCode() == 305 || con.getResponseCode() == 308 || con.getResponseCode() == 307) {
                             Logger.log(ConsoleColors.CODES.ANSI_YELLOW + file + ":" + url + "/" + line + " code: " + new String(String.valueOf(con.getResponseCode())));
+
+                        //Client Error
+                        } else if (con.getResponseCode() == 400 || con.getResponseCode() == 401 || con.getResponseCode() == 402 || con.getResponseCode() == 403 || con.getResponseCode() == 404 || con.getResponseCode() == 406 || con.getResponseCode() == 409 || con.getResponseCode() == 411 || con.getResponseCode() == 414 || con.getResponseCode() == 423|| con.getResponseCode() == 426 || con.getResponseCode() == 429 || con.getResponseCode() == 451) {
+                            Logger.log(ConsoleColors.CODES.ANSI_RED + file + ":" + url + "/" + line + " code: " + new String(String.valueOf(con.getResponseCode())));
+
+                        //Server Error
+                        } else if (con.getResponseCode() == 500 || con.getResponseCode() == 501 || con.getResponseCode() == 502 || con.getResponseCode() == 503 || con.getResponseCode() == 504 || con.getResponseCode() == 505 || con.getResponseCode() == 506 || con.getResponseCode() == 507 || con.getResponseCode() == 508 || con.getResponseCode() == 509 || con.getResponseCode() == 510|| con.getResponseCode() == 511) {
+                            Logger.log(ConsoleColors.CODES.ANSI_PURPLE + file + ":" + url + "/" + line + " code: " + new String(String.valueOf(con.getResponseCode())));
+
+
                         } else {
-                            Logger.log(ConsoleColors.CODES.ANSI_YELLOW + file + ":" + url + "/" + line + " code: " + new String(String.valueOf(con.getResponseCode())));
+
+                            //All others codes
+                            Logger.log(file + ":" + url + "/" + line + " code: " + new String(String.valueOf(con.getResponseCode())));
                         }
+                        //End of path print
+
+
+
+
                         file++;
+
+
                         //Save to log file if response code not 404, 403, 400
-                        if (con.getResponseCode() != 404 && con.getResponseCode() != 400 && con.getResponseCode() != 403 && con.getResponseCode() != 301) {
+                        if (con.getResponseCode() != 404 && con.getResponseCode() != 400 && con.getResponseCode() != 403 && con.getResponseCode() != 301 && con.getResponseCode() != 302 && con.getResponseCode() != 308 && con.getResponseCode() != 301 && con.getResponseCode() != 509 && con.getResponseCode() != 429 && con.getResponseCode() != 500) {
                             foundFiles++;
                             fileUtils.saveMessageLog(url + "/" + line + " - " + new String(String.valueOf(con.getResponseCode())), "scanned_logs/" + validator.urlStrip(url) + ".log");
                         }
--- src/main/java/xyz/becvar/websitescanner/utils/console/Logger.java
+++ src/main/java/xyz/becvar/websitescanner/utils/console/Logger.java
@@ -5,7 +5,7 @@ import xyz.becvar.websitescanner.Main;
 public class Logger {
 
     //Main app prefix string
-    public static String Prefix = ConsoleColors.CODES.ANSI_RED + "[" + ConsoleColors.CODES.ANSI_BLUE + Main.APP_NAME + ConsoleColors.CODES.ANSI_RED + "]" + ConsoleColors.CODES.ANSI_YELLOW + ": " + ConsoleColors.CODES.ANSI_GREEN;
+    public static String Prefix = ConsoleColors.CODES.ANSI_RED + "[" + ConsoleColors.CODES.ANSI_BLUE + Main.APP_NAME + ConsoleColors.CODES.ANSI_RED + "]" + ConsoleColors.CODES.ANSI_YELLOW + ": " + ConsoleColors.CODES.ANSI_CYAN;
 
     public static void log(String msg) {
         System.out.println(Prefix + msg);
